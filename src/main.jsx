76aae1909d64cf884f281ae68c22b14d2808fcbd
Manuel <damanuel2007@gmail.com>
2023-10-21 08:08:41 -0300

    redux thunk

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux' 
 
-import { Provider } from 'react-redux'
-
+import store from './store'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-import noteReducer from './reducers/noteReducer'
-
-const store = configureStore({
-    reducer: {
-        notes: noteReducer,
-        filter: filterReducer
-    }
-})
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <Provider store={store}>
     <App />
-
   </Provider>
 )
